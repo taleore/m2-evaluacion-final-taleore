6ab109a3704773ce7787a62cf424854b64b1618c
taleore <taleore@gmail.com>
2019-08-08 18:20:05 +0200

    add js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,26 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const btn = document.querySelector(".js-btn-search");
+let series = [];
+
+const getDatafromServer = ev => {
+  ev.preventDefault();
+  const url = "http://api.tvmaze.com/search/shows?q=nova";
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+};
+
+const formatData = data => {
+  const result = [];
+  for (const item of data) {
+    result.push({
+      name: item.show.name,
+      image: item.image.medium
+    });
+  }
+};
+
+btn.addEventListener("click", getDatafromServer);
