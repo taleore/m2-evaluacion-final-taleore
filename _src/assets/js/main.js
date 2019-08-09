2ccafcbf45623bc8976b7cfc34ac33cedb116277
taleore <taleore@gmail.com>
2019-08-09 10:39:31 +0200

    Add paint series

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,26 +1,43 @@
 "use strict";
 
 const btn = document.querySelector(".js-btn-search");
+const inputUser = document.querySelector(".js-btn-serie");
 let series = [];
 
 const getDatafromServer = ev => {
   ev.preventDefault();
-  const url = "http://api.tvmaze.com/search/shows?q=nova";
+  const url = `http://api.tvmaze.com/search/shows?q=${inputUser.value}`;
   return fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      console.log(
+        "Fetch data from server and return it as JSON >>> Return",
+        data
+      );
+      data = formatData(data);
+      saveDataInSeries(data);
+      //paintSeries();
     });
 };
 
 const formatData = data => {
-  const result = [];
-  for (const item of data) {
-    result.push({
-      name: item.show.name,
-      image: item.image.medium
+  const searchResult = [];
+  for (let i = 0; i < data.length; i = i + 1) {
+    searchResult.push({
+      image: data[i].show.image.medium,
+      name: data[i].show.name
     });
   }
+  console.log(
+    "Format JSON data and return it as array >>> Return",
+    searchResult
+  );
+  return searchResult;
+};
+
+const saveDataInSeries = data => {
+  series = data;
+  console.log("Save data in series array >> series", series);
 };
 
 btn.addEventListener("click", getDatafromServer);
